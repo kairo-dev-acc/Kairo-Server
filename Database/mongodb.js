fedfabfe64d08aaba1a4b7e72081fb099bd73679
nosaozemoyaefeanthony <nosaozemoyaefeanthony@gmail.com>
2025-11-01 15:29:41 +0100

    Update mongodb.js

--- Database/mongodb.js
+++ Database/mongodb.js
@@ -1,9 +1,19 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2025 Kairo. All rights reserved.
+//
+// This file is part of a proprietary project owned by Kairo.
+// Unauthorized copying, modification, distribution, or use of this file,
+// in whole or in part, is strictly prohibited without written permission.
+// -----------------------------------------------------------------------------
+// *MongoDB Connection Setup
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const MONGODB_URI_one = process.env.MONGODB_URI_one || 'mongodb://localhost:27017/mydatabase'; 
+// *Load MongoDB connection URIs from environment variables
+const MONGODB_URI_one = process.env.MONGODB_URI_one; 
 // const MONGODB_URI_two = 'mongodb://localhost:27017/mydatabase'; 
 
+// *Function to connect to MongoDB
 const connectDB = async () => {
 
     if(!MONGODB_URI_one) {
