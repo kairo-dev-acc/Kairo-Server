2caebe5affa56ccc3d0b088ffe1366c77dbeca70
nosaozemoyaefeanthony <nosaozemoyaefeanthony@gmail.com>
2025-11-01 15:55:42 +0100

    Create User.model.js

---
+++ Models/User.model.js
@@ -0,0 +1,65 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2025 Kairo. All rights reserved.
+//
+// This file is part of a proprietary project owned by Kairo.
+// Unauthorized copying, modification, distribution, or use of this file,
+// in whole or in part, is strictly prohibited without written permission.
+// -----------------------------------------------------------------------------
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    middleName: {
+      type: String,
+      required: false,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    overAllRating: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 5,
+    },
+    level: {
+      type: Number,
+      min: 1,
+      max: 3,
+      default: 1,
+    },
+    vetted: {
+      type: Boolean,
+      default: false,
+    },
+    idVerified: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
